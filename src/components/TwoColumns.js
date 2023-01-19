4219334bb3b98c7661263c08ab82885773ae5d17
silqwer <silqwer@gmail.com>
2023-01-19 11:01:30 +0900

    refactor: 이미지 지연 로딩 적용

--- src/components/TwoColumns.js
+++ src/components/TwoColumns.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function TwoColumns(props) {
+
+	useEffect(()=>{
+		const options = {}
+		const callback = (entries, observer) => {
+			entries.forEach(entry => {
+				if(entry.isIntersecting){
+					console.log('is intersecting:', entry.target.dataset.src)
+					entry.target.src = entry.target.dataset.src
+					observer.unobserve(entry.target)
+				}
+			})
+		}
+		const intersectionObserver = new IntersectionObserver(callback, options)
+		intersectionObserver.observe(props.columns[0].ref.current)
+
+		return () => intersectionObserver.disconnect()
+	},[props.columns])
+	
 	return (
 		<div className="TwoColumns py-16" style={{backgroundColor: props.bgColor}}>
 			<div className={"flex flex-col sm:flex-row container mx-auto" + (props.mobileReverse ? " flex-col-reverse" : "")}>
