ad61a2a57d61b3b33d6681d6fe428581acaab8b0
silqwer <silqwer@gmail.com>
2023-01-31 11:36:45 +0900

    refactor: 객체를 새로 만들지 않도록 반환 값 나누기

--- src/containers/ImageModalContainer.js
+++ src/containers/ImageModalContainer.js
@@ -3,12 +3,11 @@ import { useSelector } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const { modalVisible, bgColor, src, alt } = useSelector(state => ({
-    modalVisible: state.imageModal.modalVisible,
-    bgColor: state.imageModal.bgColor,
-    src: state.imageModal.src,
-    alt: state.imageModal.alt,
-  }));
+  const modalVisible = useSelector(state => state.ImageModal.modalVisible)
+  const bgColor = useSelector(state => state.ImageModal.bgColor)
+  const src = useSelector(state => state.ImageModal.src)
+  const alt = useSelector(state => state.ImageModal.alt)
+  
 
   return (
     <ImageModal
