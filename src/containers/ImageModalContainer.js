1571d60db4193e4e39423194770fc9a8be04d7d0
silqwer <silqwer@gmail.com>
2023-01-31 12:03:01 +0900

    refactor: Equality Function 적용

--- src/containers/ImageModalContainer.js
+++ src/containers/ImageModalContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const modalVisible = useSelector(state => state.ImageModal.modalVisible)
-  const bgColor = useSelector(state => state.ImageModal.bgColor)
-  const src = useSelector(state => state.ImageModal.src)
-  const alt = useSelector(state => state.ImageModal.alt)
+  const { modalVisible, bgColor, src, alt } = useSelector(state => ({
+    modalVisible: state.imageModal.modalVisible,
+    bgColor: state.imageModal.bgColor,
+    src: state.imageModal.src,
+    alt: state.imageModal.alt,
+  }),
+    shallowEqual
+  );
   
 
   return (
